7dd09c5f60dd49b70e198b7794d81e0e3b74ef5d
25matheus <41026674+25matheus@users.noreply.github.com>
2022-07-19 16:02:49 -0400

    Initial commit

---
+++ applogin/views.py
@@ -0,0 +1,50 @@
+import email
+from wsgiref.util import request_uri
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+def home(request):
+    return render(request, "authentication/index.html")
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        myuser = User.objects.create_user(username, email, password1)
+        myuser.first_name = firstname
+        myuser.last_name = lastname
+        myuser.save()
+
+        messages.success(request, "Your account has been created!")
+        return redirect("signin")
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password1 = request.POST['password1']
+
+        user = authenticate(username=username, password=password1)
+
+        if user is not None:
+            login(request, user)
+            firstname = user.first_name
+            return render(request, "authentication/index.html", {'fname':firstname})
+        else:
+            messages.error(request, "incorrect credentials")
+            return redirect('home')
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out")
+    return redirect('home')
